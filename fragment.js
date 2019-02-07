0a5ddf63b9066b4393672c2280f2055dcd7c8754
Rohit Tiwari <rohittiwarirvt@gmail.com>
2019-02-07 11:50:33 +0530

    a commit for quick spa

---
+++ fragment.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const pathname = url.parse(req.url).pathname;
+    const jsHeader = {'Content-Type': 'application/javascript'};
+    switch(pathname) {
+        case '/public/bundle.js':
+            res.writeHead(200, jsHeader);
+            return fs.createReadStream('./public/bundle.js').pipe(res); 
+        case '/public/0.bundle.js': 
+            res.writeHead(200, jsHeader);
+            return fs.createReadStream('./public/0.bundle.js').pipe(res); 
+        case '/public/1.bundle.js': 
+            res.writeHead(200, jsHeader);
+            return fs.createReadStream('./public/1.bundle.js').pipe(res); 
+        case '/css/app.css': 
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            return fs.createReadStream('./css/app.css').pipe(res);
+        case '/favicon.ico': 
+            res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+            return res.end('');
+        default:
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Link': `<http://localhost:8081/css/app.css>;rel="stylesheet",` +
+                        `<http://localhost:8081/public/bundle.js>; rel="fragment-script"`
+            });
+            return res.end(``);
+    }
+    
+});
+
+server.listen(8081, () => {
+    console.log('SPA Fragment Server started at 8081');
+});
---
+++ node_modules/node-tailor/lib/fetch-template.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
+
+const TEMPLATE_ERROR = 0;
+const TEMPLATE_NOT_FOUND = 1;
+
+class TemplateError extends Error {
+    constructor(...args) {
+        super(...args);
+
+        let code = TEMPLATE_ERROR;
+        if (args.length > 0 && args[0].code === 'ENOENT') {
+            code = TEMPLATE_NOT_FOUND;
+            this.presentable = 'template not found';
+        }
+        this.code = code;
+    }
+}
+
+const readFile = path => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf-8', (err, data) => {
+            if (err) {
+                reject(new TemplateError(err));
+                return;
+            }
+            resolve(data);
+        });
+    });
+};
+
+module.exports = function fetchTemplate (templatesPath, baseTemplateFn) {
+    return (request, parseTemplate) => {
+        const pathname = url.parse(request.url, true).pathname;
+        const templatePath = path.join(templatesPath, pathname) + '.html';
+        return readFile(templatePath)
+            .then(baseTemplate => {
+                if (typeof baseTemplateFn !== 'function') {
+                    return parseTemplate(baseTemplate);
+                }
+
+                const templateName = baseTemplateFn(pathname);
+                if (!templateName) {
+                    return parseTemplate(baseTemplate);
+                }
+
+                const pageTemplate = baseTemplate;
+                const baseTemplatePath = path.join(templatesPath, templateName) + '.html';
+                return readFile(baseTemplatePath)
+                    .then(baseTemplate => parseTemplate(baseTemplate, pageTemplate));
+            });
+    };
+};
+
+module.exports.TEMPLATE_ERROR = TEMPLATE_ERROR;
+module.exports.TEMPLATE_NOT_FOUND = TEMPLATE_NOT_FOUND;
